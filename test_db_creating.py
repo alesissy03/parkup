0697150dd55df02ad22e04eac10650491e71a3ce
Paula Rusen <paula.rusen03@gmail.com>
2025-12-05 13:17:14 +0200

    Removed the test file for the database

--- test_db_creating.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from app.extensions import db
-from app.models import User, ParkingLot, ParkingSpot, Reservation
-
-app = create_app()
-
-with app.app_context():
-    print("Tables detected:")
-    print(db.inspect(db.engine).get_table_names())
